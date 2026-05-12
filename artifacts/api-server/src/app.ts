9b3fc00f7697c9693a67c77f1dae4cd0e37fa10f
Aashvi2120 <aashvivijaybhatiya@gmail.com>
2026-05-12 23:17:06 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -31,6 +31,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (_req: any, res: any) => {
+  res.send("Wardrobe Stylist API Running");
+});
+
 app.use("/api", router);
 
 export default app;
